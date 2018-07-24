9de95db76c896157ea15bd596e453a2c1a0cb420
Ubu <ubald2b@gmail.com>
2018-07-25 01:31:01 +0200

    Cleanup in plopfile

--- plopfile.js
+++ plopfile.js
@@ -1,24 +1,23 @@
 module.exports = plop => {
-    // create your generators here
-    plop.setGenerator('Component', {
-        description: 'Component directory',
+    plop.setGenerator(`Component`, {
+        description: `Component directory`,
         prompts: [{
-            type: 'input',
-            name: 'componentName',
-            message: 'What is the component name?'
+            type: `input`,
+            name: `componentName`,
+            message: `What is the component name?`,
         }],
         actions: [{
-            type: 'add',
-            path: 'src/components/{{dashCase componentName}}/{{dashCase componentName}}.vue',
-            templateFile: 'plop-templates/component/template.hbs'
+            type: `add`,
+            path: `src/components/{{dashCase componentName}}/{{dashCase componentName}}.vue`,
+            templateFile: `plop-templates/component/template.hbs`,
         }, {
-            type: 'add',
-            path: 'src/components/{{dashCase componentName}}/{{dashCase componentName}}.scss',
-            templateFile: 'plop-templates/component/style.hbs'
+            type: `add`,
+            path: `src/components/{{dashCase componentName}}/{{dashCase componentName}}.scss`,
+            templateFile: `plop-templates/component/style.hbs`,
         }, {
-            type: 'add',
-            path: 'src/components/{{dashCase componentName}}/{{dashCase componentName}}.js',
-            templateFile: 'plop-templates/component/script.hbs'
-        }]
-    });
-};
+            type: `add`,
+            path: `src/components/{{dashCase componentName}}/{{dashCase componentName}}.js`,
+            templateFile: `plop-templates/component/script.hbs`,
+        }],
+    })
+}
