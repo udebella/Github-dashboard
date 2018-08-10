dfa7c125498b6037a273316e6564fed7b161c57d
Ubu <ubald2b@gmail.com>
2018-08-11 01:03:09 +0200

    Improve display for header component

--- src/components/dashboard-header/dashboard-header.vue
+++ src/components/dashboard-header/dashboard-header.vue
@@ -1,9 +1,12 @@
 <template>
-	<div>
-		<div data-test="title">
+	<div class="header">
+		<div 
+			class="title" 
+			data-test="title">
 			Github Dashboard
 		</div>
-		<a 
+		<a
+			target="_blank"
 			href="https://github.com/udebella/Github-dashboard" 
 			data-test="sources">View sources</a>
 	</div>
