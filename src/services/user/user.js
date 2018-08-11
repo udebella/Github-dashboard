7e3682dc4705729a4a9689bb42f319bec2546f93
Ubu <ubald2b@gmail.com>
2018-08-11 19:21:41 +0200

    Login to github using token

--- src/services/user/user.js
+++ src/services/user/user.js
@@ -1,4 +1,28 @@
-export const buildUserService = () => {
+const query = ``
 
-	return {}
+export const buildUserService = graphqlClient => {
+	const login = async token => {
+		try {
+			const {data} = await graphqlClient.request(query)
+			return ({
+				success: {
+					login: data.viewer.login,
+					token: token,
+				},
+			})
+		} catch ({response}) {
+			const {message, status} = response
+			return ({
+				error: {
+					message,
+					code: status,
+				},
+			})
+		}
+
+	}
+
+	return {
+		login,
+	}
 }
--- src/services/user/user.spec.js
+++ src/services/user/user.spec.js
@@ -4,15 +4,58 @@ import {buildUserService} from "./user"
 
 
 describe(`User service`, () => {
-	let userService
+	let userService, fakeGraphQLClient
 
 	beforeEach(() => {
-		userService = buildUserService()
+		fakeGraphQLClient = {
+			request: stub(),
+		}
+		userService = buildUserService(fakeGraphQLClient)
 	})
 
 	describe(`Initialization`, () => {
 		it(`should init properly`, () => {
 			expect(userService).to.exist
+		})
+	})
+
+	describe(`Login`, () => {
+		it(`should validate given token to github api`, async () => {
+			// Given
+			fakeGraphQLClient.request.returns({
+				data: {
+					viewer: {
+						login: `user`,
+					},
+				},
+			})
+
+			// When
+			const loggedUser = await userService.login(`token`)
+
+			// Then
+			expect(loggedUser).to.deep.equals({
+				success: {
+					login: `user`,
+					token: `token`,
+				},
+			})
+		})
+
+		it(`should handle wrong token`, async () => {
+			// Given
+			fakeGraphQLClient.request.throws({response:{message:`Bad credentials`,status:401}})
+
+			// When
+			const loggedUser = await userService.login(`token`)
+
+			// Then
+			expect(loggedUser).to.deep.equals({
+				error: {
+					code: 401,
+					message: `Bad credentials`,
+				},
+			})
 		})
 	})
 })
