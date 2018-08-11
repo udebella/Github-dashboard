515a50cb0354ca06c4e666c5db3fa8b150588c1a
Ubu <ubald2b@gmail.com>
2018-08-12 00:58:03 +0200

    Retrieve session token before each request

--- src/services/graphql/graphql-client.js
+++ src/services/graphql/graphql-client.js
@@ -1,10 +1,28 @@
 import {GraphQLClient} from 'graphql-request'
 import {token} from './token.hidden.js' // TODO make a component that asks the use for a personal token as we cannot use github login in a frontend app (it would need a server)
 
+export const getClient = (graphQlClientBuilder, session) => {
+	const client = graphQlClientBuilder(`/graphql`, {
+		headers: {
+			Authorization: `token ${session.getUser()}`,
+		},
+	})
+
+	return {
+		request: client.request,
+	}
+}
+
+/**
+ * @deprecated
+ */
 const defaultClient = new GraphQLClient(`/graphql`, {
 	headers: {
 		Authorization: `token ${token}`,
 	},
 })
 
+/**
+ * @deprecated
+ */
 export const request = async (query, client = defaultClient) => client.request(query)
--- src/services/graphql/graphql-client.spec.js
+++ src/services/graphql/graphql-client.spec.js
@@ -1,16 +1,46 @@
-import {request} from "./graphql-client"
+import {getClient} from "./graphql-client"
 import {expect} from 'chai'
-import {stub} from 'sinon'
+import {spy, stub} from 'sinon'
 
 describe(`Service: graphql-client`, () => {
-    it(`should call request method from graphql client`, async () => {
-        // Given
-        const fakeRequest = stub()
+	let graphQlClient, graphQlClientBuilder, fakeRequest, fakeSession
 
-        // When
-        await request(`fakeQuery`, {request: fakeRequest})
+	beforeEach(() => {
+		fakeSession = {
+			getUser: stub(),
+		}
+		fakeRequest = stub()
+		graphQlClientBuilder = spy(() => ({
+			request: fakeRequest,
+		}))
+		graphQlClient = getClient(graphQlClientBuilder, fakeSession)
+	})
 
-        // Then
-        expect(fakeRequest).to.have.been.calledWith(`fakeQuery`)
-    })
+	describe(`Initialization`, () => {
+		it(`should retrieve user from session before making a request`, () => {
+			expect(fakeSession.getUser).to.have.been.called
+		})
+
+		it(`should build a graphqlClient properly`, () => {
+			fakeSession.getUser.returns(`userToken`)
+
+			graphQlClient = getClient(graphQlClientBuilder, fakeSession)
+
+			expect(graphQlClientBuilder).to.have.been.calledWith(`/graphql`, {
+				headers: {
+					Authorization: `token userToken`,
+				},
+			})
+		})
+	})
+
+	describe(`Method request`, () => {
+		it(`should call request method from graphql client`, () => {
+			// When
+			graphQlClient.request(`fakeQuery`)
+
+			// Then
+			expect(fakeRequest).to.have.been.calledWith(`fakeQuery`)
+		})
+	})
 })
