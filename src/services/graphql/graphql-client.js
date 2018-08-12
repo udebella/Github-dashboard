8baa63ad8cfc4a177598f242d1046fb3b787c555
Ubu <ubald2b@gmail.com>
2018-08-12 17:28:25 +0200

    Fix default builder for graphql client was not called properly

--- src/services/graphql/graphql-client.js
+++ src/services/graphql/graphql-client.js
@@ -2,7 +2,7 @@ import {GraphQLClient} from 'graphql-request'
 import {buildSessionService} from "../session/session"
 
 // TODO Is it possible to test that?
-const defaultBuilder = (...args) => new GraphQLClient(args)
+const defaultBuilder = (...args) => new GraphQLClient(...args)
 
 export const request = async (query, {builder = defaultBuilder, session = buildSessionService()} = {}) => {
 	const {token} = session.getUser()
