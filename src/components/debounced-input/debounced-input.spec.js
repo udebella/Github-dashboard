26f7ee868d99a563b990fb630bd9e5d4df29a75e
Ubu <ubald2b@gmail.com>
2018-08-15 19:42:22 +0200

    Refactoring remove before each for debounced input tests

--- src/components/debounced-input/debounced-input.spec.js
+++ src/components/debounced-input/debounced-input.spec.js
@@ -3,18 +3,15 @@ import {shallowMount} from '@vue/test-utils'
 import DebouncedInput from './debounced-input.vue'
 
 describe(`DebouncedInput component`, () => {
-	let debouncedInput
-
-	beforeEach(() => {
-		debouncedInput = shallowMount(DebouncedInput)
-	})
-
 	describe(`Initialization`, () => {
 		it(`should have the right component name`, () => {
+			const debouncedInput = shallowMount(DebouncedInput)
+
 			expect(debouncedInput.name()).to.equal(`debounced-input`)
 		})
 
 		it(`should display a text input`, () => {
+			const debouncedInput = shallowMount(DebouncedInput)
 			const input = debouncedInput.find(`input`)
 
 			expect(input.exists()).to.be.true
