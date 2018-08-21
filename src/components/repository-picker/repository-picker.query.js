30948a2719198ed7868f63d2d5d18054d4dfdfbf
Ubu <ubald2b@gmail.com>
2018-08-22 01:39:29 +0200

    Order repositories by popularity when selecting

--- src/components/repository-picker/repository-picker.query.js
+++ src/components/repository-picker/repository-picker.query.js
@@ -1,6 +1,6 @@
 export const query = username => `{
   repositoryOwner(login: ${username}) {
-  	repositories(first: 20) {
+  	repositories(first: 20, orderBy: {field: STARGAZERS, direction: DESC}) {
       nodes {
         name
         owner {
