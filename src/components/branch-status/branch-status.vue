ff05475a80eb95bb9383dd7050c34703a0dddcff
Ubu <ubald2b@gmail.com>
2018-10-14 16:47:25 +0200

    Removed branch status component as we don't need it anymore

--- src/components/branch-status/branch-status.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<build-statuses 
-		:statuses="statusesList" 
-		data-test="statuses"/>
-</template>
-
-<script src="./branch-status.js"></script>
-<style src="./branch-status.scss" scoped></style>
