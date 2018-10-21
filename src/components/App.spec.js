ffcfe07f7d1dfa03c78cd26c49403f82c875db44
Ubu <ubald2b@gmail.com>
2018-10-21 15:31:55 +0200

    Added missing tests on app component

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -17,5 +17,13 @@ describe(`Component App`, () => {
 		it(`should display a header`, () => {
 			expect(app.find({name: `dashboard-header`}).exists()).to.be.true
 		})
+
+		it(`should display a way to configure github api url`, () => {
+			expect(app.find({name: `github-api-config`}).exists()).to.be.true
+		})
+
+		it(`should display the list of repositories`, () => {
+			expect(app.find({name: `repository-list`}).exists()).to.be.true
+		})
 	})
 })
