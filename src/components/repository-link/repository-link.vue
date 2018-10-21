c6ba4c8c1c3d022fec97d14f688e1116d6cf066a
Ubu <ubald2b@gmail.com>
2018-10-21 23:12:47 +0200

    Removed dead code

--- src/components/repository-link/repository-link.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<a
-		:href="url"
-		class="link"
-		target="_blank">{{ name }}</a>
-</template>
-
-<script src="./repository-link.js"></script>
-<style src="./repository-link.scss" scoped></style>
