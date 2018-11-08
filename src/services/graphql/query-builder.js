26a050a524a21d54cdb9cc7939018ad8df634a9f
Ubu <ubald2b@gmail.com>
2018-11-08 23:48:45 +0100

    Refactoring on query builder

--- src/services/graphql/query-builder.js
+++ src/services/graphql/query-builder.js
@@ -5,10 +5,9 @@ export const buildRepositoriesQuery = fragment => repositoryList => {
 }
 
 export const buildViewerQuery = fragment => {
-	if (!fragment) {
-		return `${rateLimitFragment}\n\nquery {\n\t...rateLimit\n}`
-	}
-	return `${rateLimitFragment}\n\n${fragment}\n\nquery {\n\t...rateLimit\n\tviewer {...viewer}\n}`
+	const formattedFragment = fragment ? `\n\n${fragment}` : ``
+	const query = fragment ? `\n\tviewer {...viewer}` : ``
+	return `${rateLimitFragment}${formattedFragment}\n\nquery {\n\t...rateLimit${query}\n}`
 }
 
 const rateLimitFragment = `fragment rateLimit on Query {
@@ -21,8 +20,8 @@ const rateLimitFragment = `fragment rateLimit on Query {
 }`
 
 const repositoryListFragment = repositoryList => {
-	const temp = repositoryList
+	const fragmentContent = repositoryList
 		.map(({owner, name}, index) => `\trep_${index}: repository(owner: "${owner}", name: "${name}") {...repository}`)
 		.reduce((previous, current) => `${previous}\n${current}`)
-	return `fragment repositoryList on Query {\n${temp}\n}`
+	return `fragment repositoryList on Query {\n${fragmentContent}\n}`
 }
