c76a606bf2d75b6f104f196bbeeb883426575c12
Ubu <ubald2b@gmail.com>
2018-11-09 01:06:44 +0100

    Call graphql api to retrieve list of opened pull request by the viewer

--- src/components/viewer-pull-request-list/viewer-pull-request-list.js
+++ src/components/viewer-pull-request-list/viewer-pull-request-list.js
@@ -1,3 +1,57 @@
+import {request as defaultRequest} from "../../services/graphql/graphql-client";
+import {buildViewerQuery} from "../../services/graphql/query-builder";
+
 export default {
 	name: `viewer-pull-request-list`,
+	props: {
+		request: {
+			type: Function,
+			default: defaultRequest,
+		},
+		queryBuilder: {
+			type: Function,
+			default: buildViewerQuery,
+		},
+	},
+	asyncComputed: {
+		pullRequests: {
+			async get() {
+				await this.request(this.queryBuilder(viewerFragment))
+			},
+			default: [],
+		},
+	},
 }
+
+
+export const viewerFragment = `fragment viewer on User {
+  pullRequests(states: OPEN, last: 20) {
+    nodes {
+      title
+      url
+      comments {
+        totalCount
+      }
+      createdAt
+      updatedAt
+      reviews(first: 1) {
+        totalCount
+      }
+      state
+      commits(last: 1) {
+        nodes {
+          commit {
+            status {
+            contexts {
+			    state
+			    context
+			    targetUrl
+			  }
+              state
+            }
+          }
+        }
+      }
+    }
+  }
+}`
--- src/components/viewer-pull-request-list/viewer-pull-request-list.spec.js
+++ src/components/viewer-pull-request-list/viewer-pull-request-list.spec.js
@@ -1,21 +1,42 @@
 import {expect} from 'chai'
 import {shallowMount} from '@vue/test-utils'
 import ViewerPullRequestList from './viewer-pull-request-list.vue'
+import flushPromises from 'flush-promises'
+import {stub} from 'sinon'
+import {viewerFragment} from './viewer-pull-request-list'
 
 describe(`ViewerPullRequestList component`, () => {
-	let viewerPullRequestList
+	let stubs
 
 	beforeEach(() => {
-		viewerPullRequestList = shallowMount(ViewerPullRequestList)
+		const store = {
+			state: {watchedRepositories: [{name: `repository`, owner: `user`}]},
+		}
+		stubs = {
+			store,
+			queryBuilder: stub(),
+			request: stub(),
+		}
 	})
 
 	describe(`Initialization`, () => {
 		it(`should have the right component name`, () => {
+			const viewerPullRequestList = shallowMount(ViewerPullRequestList, {store: stubs.store, propsData: stubs})
+
 			expect(viewerPullRequestList.name()).to.equal(`viewer-pull-request-list`)
 		})
 
-		it(`should display the component`, () => {
-			expect(viewerPullRequestList.find(`div`).text()).to.equal(`Test component`)
+		it(`should call graphql api to retrieve data over the list of repositories`, async () => {
+			// Given
+			stubs.queryBuilder.returns(`queryBuilt`)
+
+			// When
+			shallowMount(ViewerPullRequestList, {store: stubs.store, propsData: stubs})
+
+			// Then
+			await flushPromises()
+			expect(stubs.request).to.have.been.calledWith(`queryBuilt`)
+			expect(stubs.queryBuilder).to.have.been.calledWith(viewerFragment)
 		})
 	})
 })
