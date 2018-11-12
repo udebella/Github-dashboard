ca9495b3c019ec04c8ad747e9f5ec0ccef899dbd
Ubu <ubald2b@gmail.com>
2018-11-12 12:42:32 +0100

    Extract main informations from pull request response from graphql

--- src/services/pull-request/pull-request.js
+++ src/services/pull-request/pull-request.js
@@ -1,1 +1,8 @@
-export const extractHttp = () => ([])
+export const extractHttp = ({pullRequests}) => pullRequests.nodes
+	.map(({title, url, createdAt}) => ({
+		prTitle: title,
+		prUrl: url,
+		statuses: [],
+		buildStatus: `NO_STATUS`,
+		creationDate: new Date(createdAt),
+	}))
--- src/services/pull-request/pull-request.spec.js
+++ src/services/pull-request/pull-request.spec.js
@@ -7,4 +7,42 @@ describe(`Pull request service`, () => {
 
 		expect(response).to.deep.equals([])
 	})
+
+	it(`should return a formatted response when pull request was not build`, () => {
+		const httpResponse = {
+			pullRequests: {
+				nodes: [
+					{
+						title: `Fix wheel/touch browser locking in IE and Safari`,
+						url: `https://github.com/facebook/react/pull/9333`,
+						comments: {totalCount: 36},
+						reviews: {totalCount: 39},
+						updatedAt: `2018-10-25T01:36:27Z`,
+						createdAt: `2018-10-20T00:00:00Z`,
+						state: `OPEN`,
+						commits: {
+							nodes: [{
+								commit: {
+									status: {
+										contexts: [],
+										state: null,
+									},
+								},
+							}],
+						},
+					},
+				],
+			},
+		}
+
+		const response = extractHttp(httpResponse)
+
+		expect(response).to.deep.equals([{
+			prTitle: `Fix wheel/touch browser locking in IE and Safari`,
+			prUrl: `https://github.com/facebook/react/pull/9333`,
+			creationDate: new Date(`2018-10-20T00:00:00Z`),
+			buildStatus: `NO_STATUS`,
+			statuses: [],
+		}])
+	})
 })
