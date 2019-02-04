0e5fff1bfe6c08d028355f9ca05859ef4f7625f0
Ubu <ubald2b@gmail.com>
2019-02-05 00:24:20 +0100

    Update dependencies

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,1 +1,5 @@
 require('jsdom-global')(undefined, {url: 'https://example.org/'})
+
+// https://github.com/vuejs/vue-test-utils/issues/936
+// FIXME "TypeError: Super expression must either be null or a function" because of prettier.
+window.Date = Date
