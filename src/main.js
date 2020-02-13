960b0522b90a3a5fe3efb97b95c613c2b18d9293
Ubu <ubald2b@gmail.com>
2020-02-14 00:51:21 +0100

    Removed unused dependency (due to previous refactoring with network-polling component)

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './components/App'
 import {store} from './store/store'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome/index.es'
-import AsyncComputed from 'vue-async-computed'
 
-Vue.use(AsyncComputed)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
--- src/main.spec.js
+++ src/main.spec.js
@@ -2,9 +2,7 @@ import * as chai from 'chai'
 import * as sinonChai from 'sinon-chai'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import AsyncComputed from 'vue-async-computed'
 
 Vue.use(Vuex)
-Vue.use(AsyncComputed)
 chai.use(sinonChai)
 Vue.component('font-awesome-icon', {})
