e5ac3ce22bc940f2c4ded5063b396bf3ddc05cef
Ubu <ubald2b@gmail.com>
2020-02-19 00:59:51 +0100

    Refactor only call notificationApi once

--- src/services/pull-request-notifications/pull-request-notifications.js
+++ src/services/pull-request-notifications/pull-request-notifications.js
@@ -3,15 +3,16 @@ import {notificationApi} from '../notifications/notification'
 const defaults = {notificationApi: notificationApi}
 const pullRequestNotifications = ({notificationApi} = defaults) => {
 	let alreadyNotified = []
+	const notificationService = notificationApi()
 
-	const newList = (pullRequests) => {
+	const newList = pullRequests => {
 		const prToNotify = pullRequests
 			.filter(({title}) => !alreadyNotified.find(notifiedPr => notifiedPr.title === title))
 		alreadyNotified = [...prToNotify]
 		if (prToNotify.length === 1) {
-			notificationApi().notify(`A new pull request was opened: ${prToNotify[0].title}`)
+			notificationService.notify(`A new pull request was opened: ${prToNotify[0].title}`)
 		} else if (prToNotify.length > 1) {
-			notificationApi().notify(`${prToNotify.length} new pull requests were opened`)
+			notificationService.notify(`${prToNotify.length} new pull requests were opened`)
 		}
 	}
 
