dedf54592bd12c84f6669d7f4093595a7645627d
Ubu <ubald2b@gmail.com>
2020-03-19 16:44:27 +0100

    Extract default store to a constant

--- src/store/store-init-plugin.js
+++ src/store/store-init-plugin.js
@@ -1,8 +1,10 @@
+const defaultStore = {
+	githubApi: 'https://api.github.com/graphql',
+	watchedRepositories: [],
+	configurationEnabled: true,
+	timeBetweenRefresh: 30,
+}
+
 export const StoreInit = ({replaceState}) => {
-	replaceState({
-		githubApi: 'https://api.github.com/graphql',
-		watchedRepositories: [],
-		configurationEnabled: true,
-		timeBetweenRefresh: 30,
-	})
+	replaceState(defaultStore)
 }
