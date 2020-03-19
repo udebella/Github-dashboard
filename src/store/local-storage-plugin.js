01dabaecfa0d8512878f87e0bddb41ad7adbc31c
Ubu <ubald2b@gmail.com>
2020-03-19 19:05:06 +0100

    Simplify local-storage-plugin

--- src/store/local-storage-plugin.js
+++ src/store/local-storage-plugin.js
@@ -1,19 +1,14 @@
 const LOCAL_STORAGE_KEY = 'github-dashboard-store'
 
 export const localStoragePlugin = defaultState => (store, storage = window.localStorage) => {
-	const stringifiedStore = storage.getItem(LOCAL_STORAGE_KEY)
+	const stringifiedStore = storage.getItem(LOCAL_STORAGE_KEY) || '{}'
 	const handler = (_, state) => {
 		storage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
 	}
 
-	if (stringifiedStore) {
-		const localStorageStore = JSON.parse(stringifiedStore)
-		store.replaceState({...defaultState, ...localStorageStore})
-		handler('initialize', {...defaultState, ...localStorageStore})
-	} else {
-		store.replaceState(defaultState)
-		handler('initialize', defaultState)
-	}
+	const localStorageStore = JSON.parse(stringifiedStore)
+	store.replaceState({...defaultState, ...localStorageStore})
+	handler('initialize', {...defaultState, ...localStorageStore})
 
 	store.subscribe(handler)
 }
