3c87315e7531d6a9465860cd7db4b06841396711
Ubu <ubald2b@gmail.com>
2020-03-19 16:43:50 +0100

    Replace state with default values in the store

--- src/store/store-init-plugin.js
+++ src/store/store-init-plugin.js
@@ -1,3 +1,8 @@
-export const StoreInit = () => {
-
+export const StoreInit = ({replaceState}) => {
+	replaceState({
+		githubApi: 'https://api.github.com/graphql',
+		watchedRepositories: [],
+		configurationEnabled: true,
+		timeBetweenRefresh: 30,
+	})
 }
--- src/store/store-init-plugin.spec.js
+++ src/store/store-init-plugin.spec.js
@@ -1,11 +1,33 @@
 import {expect} from 'chai'
 import {StoreInit} from './store-init-plugin'
+import {stub} from 'sinon'
 
 describe('Init store', () => {
+	let fakeStore
+
+	beforeEach(() => {
+		fakeStore = {
+			state: {},
+			replaceState: stub(),
+		}
+	})
 
 	describe('Initialization', () => {
 		it('should be a function to give to vuex', () => {
 			expect(StoreInit).to.be.a('function')
+		})
+	})
+
+	describe('Initialize the store', () => {
+		it('should initialize the store with default values when nothing is provided', () => {
+			StoreInit(fakeStore)
+
+			expect(fakeStore.replaceState).to.have.been.calledWith({
+				githubApi: 'https://api.github.com/graphql',
+				watchedRepositories: [],
+				configurationEnabled: true,
+				timeBetweenRefresh: 30,
+			})
 		})
 	})
 })
