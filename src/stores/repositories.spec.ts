eccac21040dca250fedb70793c414d8aa1adbf4c
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-24 15:21:46 +0200

    should add a repository to watch for the username

--- src/stores/repositories.spec.ts
+++ src/stores/repositories.spec.ts
@@ -8,9 +8,51 @@ describe('Repositories store', () => {
 		setActivePinia(createPinia())
 	});
 
-	it('does not watch any repository by default', () => {
+	it('should not watch any repository by default', () => {
 		const store = useRepositoryStore()
 
 		expect(store.watched).toEqual([])
 	});
+
+	describe('addRepository', () => {
+		it('should add a repository to watch for the username', () => {
+			const store = useRepositoryStore();
+			const repository = {owner: 'username', name: 'repository'}
+
+			store.addRepository(repository)
+
+			expect(store.watched).toEqual([repository])
+		})
+
+		// it('should allow to add multiple repositories for an user ordered by name', () => {
+		// 	const firstRepository = {owner: 'user', name: 'repository'}
+		// 	const store = {watchedRepositories: [firstRepository]}
+		//
+		// 	const repository = {owner: 'username', name: 'another repository'}
+		// 	addRepository(store, repository)
+		//
+		// 	expect(store.watchedRepositories).to.deep.equal([repository, firstRepository])
+		// })
+		//
+		// it('should not mutate the store', () => {
+		// 	// Given
+		// 	const watchedRepositories = [{owner: 'user', name: 'repository'}]
+		// 	const store = {watchedRepositories}
+		//
+		// 	// When
+		// 	addRepository(store, {owner: 'username', name: 'another repository'})
+		//
+		// 	// Then
+		// 	expect(store.watchedRepositories).not.to.equal(watchedRepositories)
+		// })
+		//
+		// it('should keep previous watched repositories in the store', () => {
+		// 	const firstRepository = {owner: 'user', name: 'repository'}
+		// 	const store = {watchedRepositories: [firstRepository]}
+		//
+		// 	addRepository(store, {owner: 'user', name: 'another repository'})
+		//
+		// 	expect(store.watchedRepositories).to.contains(firstRepository)
+		// })
+	})
 });
--- src/stores/repositories.ts
+++ src/stores/repositories.ts
@@ -1,7 +1,21 @@
 import {defineStore} from 'pinia'
 
+type Repository = {
+	name: string,
+	owner: string
+}
+
+type State = {
+	watched: Repository[]
+}
+
 export const useRepositoryStore = defineStore('repository', {
- state: () => ({
+ state: (): State => ({
 	 watched: []
- })
+ }),
+	actions: {
+	 addRepository(repository: Repository) {
+		 this.watched.push(repository)
+	 }
+	}
 })
