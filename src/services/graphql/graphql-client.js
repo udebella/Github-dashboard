f55ac45be990e170c2fcd569592306402ea01add
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-24 16:07:04 +0200

    Fix graphql-client tests

--- src/services/graphql/graphql-client.js
+++ src/services/graphql/graphql-client.js
@@ -1,13 +1,13 @@
 import {GraphQLClient} from 'graphql-request'
 import {buildSessionService} from '../session/session'
-import {store as defaultStore} from '../../store/store'
+import {useConfigurationStore} from "@/stores/configuration";
 
 // TODO Is it possible to test that?
 const defaultBuilder = (...args) => new GraphQLClient(...args)
 
-export const request = async (query, {builder = defaultBuilder, session = buildSessionService(), store = defaultStore} = {}) => {
+export const request = async (query, {builder = defaultBuilder, session = buildSessionService(), store = useConfigurationStore()} = {}) => {
 	const {token} = session.getUser()
-	const client = builder(store.state.githubApi, {
+	const client = builder(store.githubApi, {
 		headers: {
 			Authorization: `token ${token}`,
 		},
