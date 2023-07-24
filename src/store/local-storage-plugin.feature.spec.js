011aec8fae7f2c4a746d1e8f3e38ba30d65a3201
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-24 16:09:47 +0200

    Disable plugin to synchronise store inside local storage

--- src/store/local-storage-plugin.feature.spec.js
+++ src/store/local-storage-plugin.feature.spec.js
@@ -1,32 +1,33 @@
-import {expect} from 'chai'
-import {Store} from 'vuex'
-import {localStoragePlugin} from './local-storage-plugin'
-
-describe('Local storage store feature', () => {
-	let store
-
-	beforeEach(() => {
-		store = new Store({
-			state: {},
-			mutations: {
-				increment(state) {
-					state.count++
-				},
-			},
-		})
-	})
-
-	afterEach(() => {
-		window.localStorage.clear()
-	})
-
-	it('should save mutation and reload them into a new store', () => {
-		localStoragePlugin({count: 1})(store)
-
-		store.commit('increment')
-		const newStore = new Store({})
-		localStoragePlugin({count: 1})(newStore)
-
-		expect(newStore.state.count).to.equals(2)
-	})
-})
+// TODO fix this to work with pinia instead
+// import {expect} from 'chai'
+// import {Store} from 'vuex'
+// import {localStoragePlugin} from './local-storage-plugin'
+//
+// describe('Local storage store feature', () => {
+// 	let store
+//
+// 	beforeEach(() => {
+// 		store = new Store({
+// 			state: {},
+// 			mutations: {
+// 				increment(state) {
+// 					state.count++
+// 				},
+// 			},
+// 		})
+// 	})
+//
+// 	afterEach(() => {
+// 		window.localStorage.clear()
+// 	})
+//
+// 	it('should save mutation and reload them into a new store', () => {
+// 		localStoragePlugin({count: 1})(store)
+//
+// 		store.commit('increment')
+// 		const newStore = new Store({})
+// 		localStoragePlugin({count: 1})(newStore)
+//
+// 		expect(newStore.state.count).to.equals(2)
+// 	})
+// })
--- src/store/local-storage-plugin.js
+++ src/store/local-storage-plugin.js
@@ -1,14 +1,15 @@
-const LOCAL_STORAGE_KEY = 'github-dashboard-store'
-
-export const localStoragePlugin = defaultState => (store, storage = window.localStorage) => {
-	const stringifiedStore = storage.getItem(LOCAL_STORAGE_KEY) || '{}'
-	const handler = (_, state) => {
-		storage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
-	}
-
-	const localStorageStore = JSON.parse(stringifiedStore)
-	store.replaceState({...defaultState, ...localStorageStore})
-	handler('initialize', {...defaultState, ...localStorageStore})
-
-	store.subscribe(handler)
-}
+// TODO fix this to work with pinia instead
+// const LOCAL_STORAGE_KEY = 'github-dashboard-store'
+//
+// export const localStoragePlugin = defaultState => (store, storage = window.localStorage) => {
+// 	const stringifiedStore = storage.getItem(LOCAL_STORAGE_KEY) || '{}'
+// 	const handler = (_, state) => {
+// 		storage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+// 	}
+//
+// 	const localStorageStore = JSON.parse(stringifiedStore)
+// 	store.replaceState({...defaultState, ...localStorageStore})
+// 	handler('initialize', {...defaultState, ...localStorageStore})
+//
+// 	store.subscribe(handler)
+// }
