c80269a0268cca91956f443be6436dfcbd8de9af
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-26 00:50:04 +0200

    Remove references to chai in github-api-config.spec.js

--- src/components/github-api-config/github-api-config.spec.js
+++ src/components/github-api-config/github-api-config.spec.js
@@ -1,52 +1,42 @@
-import {expect} from 'chai'
 import {shallowMount} from '@vue/test-utils'
 import GithubApiConfig from './github-api-config.vue'
-import {beforeEach, describe, it} from "vitest";
+import {beforeEach, describe, expect, it} from "vitest";
 import {useConfigurationStore} from "@/stores/configuration";
 import {createPinia, setActivePinia} from "pinia";
 
 describe('GithubApiConfig component', () => {
-	let githubApiConfig, store
+	let githubApiConfig
 
 	beforeEach(() => {
 		setActivePinia(createPinia())
-		store = useConfigurationStore()
-		// {
-		// 	commit: vitest.fn(),
-		// 	state: {
-		// 		configurationEnabled: true,
-		// 		githubApi: 'http://github-api',
-		// 	},
-		// }
-
 		githubApiConfig = shallowMount(GithubApiConfig)
 	})
 
 	describe('Initialization', () => {
 		it('should mount properly', () => {
-			expect(githubApiConfig.exists()).to.be.true
+			expect(githubApiConfig.exists()).toBe(true)
 		})
 
 		it('should display an input', () => {
-			expect(githubApiConfig.find('input').exists()).to.be.true
+			expect(githubApiConfig.find('input').exists()).toBe(true)
 		})
 
 		it('should display as default value the one from the store', () => {
-			store.$patch({githubApi: 'http://github-api'})
+			useConfigurationStore().$patch({githubApi: 'http://github-api'})
 			githubApiConfig = shallowMount(GithubApiConfig)
 
-			expect(githubApiConfig.find('[data-test=input]').element.value).to.equals('http://github-api')
+			expect(githubApiConfig.find('[data-test=input]').element.value).toBe('http://github-api')
 		})
 
 		it('should be displayed when configuration mode is enabled', () => {
-			expect(githubApiConfig.find('[data-test=input]').exists()).to.be.true
+			expect(githubApiConfig.find('[data-test=input]').exists()).toBe(true)
 		})
 
 		it('should not be displayed when configuration mode is disabled', () => {
-			store.$patch({configurationEnabled: false})
+			useConfigurationStore().$patch({configurationEnabled: false})
 			githubApiConfig = shallowMount(GithubApiConfig)
 
-			expect(githubApiConfig.find('[data-test=input]').exists()).to.be.false
+			expect(githubApiConfig.find('[data-test=input]').exists()).toBe(false)
 		})
 	})
 
@@ -54,7 +44,7 @@ describe('GithubApiConfig component', () => {
 		it('should save the new api in the store when changed', () => {
 			githubApiConfig.find('[data-test=input]').setValue('https://new-api')
 
-			expect(store.githubApi).toBe('https://new-api')
+			expect(useConfigurationStore().githubApi).toBe('https://new-api')
 		})
 	})
 })
