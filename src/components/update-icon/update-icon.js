78b0ce5deb15f84b2dac07c68117a78efa4166ff
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-26 23:51:47 +0200

    Simplify update icon component

--- src/components/update-icon/update-icon.js
+++ src/components/update-icon/update-icon.js
@@ -3,7 +3,4 @@ import IconComponent from "@/components/icon/icon-component.vue";
 export default {
 	name: 'update-icon',
 	components: {IconComponent},
-	data: () => ({
-		icon: 'hasUpdates',
-	}),
 }
