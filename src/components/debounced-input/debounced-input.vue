ece416ce407910e54af831fc8622047dcb841c7e
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-27 00:22:22 +0200

    Fix tests for debounced input

--- src/components/debounced-input/debounced-input.vue
+++ src/components/debounced-input/debounced-input.vue
@@ -1,8 +1,20 @@
 <template>
-	<input 
-		v-model="input" 
-		type="text">
+	<input @input="debouncedInput" type="text">
 </template>
 
-<script src="./debounced-input.js"></script>
+<script lang="ts" setup>
+import {debounce} from 'debounce' // TODO install typings for debounce dependency
+
+const emit = defineEmits<{
+	input: string
+}>()
+
+const notifyParent = (event: Event) => {
+	const target: HTMLInputElement = event.target;
+	emit('input', target.value)
+}
+
+const debouncedInput = debounce(notifyParent, 1000)
+</script>
+
 <style src="./debounced-input.scss" scoped></style>
