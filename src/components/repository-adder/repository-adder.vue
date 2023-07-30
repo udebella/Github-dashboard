00a8c1f2b383c108b0ae1db190026f2a21c501e5
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-30 22:33:21 +0200

    Fix remaining typescript errors

--- src/components/repository-adder/repository-adder.vue
+++ src/components/repository-adder/repository-adder.vue
@@ -1,13 +1,14 @@
 <template>
 	<badge v-if="configurationEnabled" data-test="button" title="Watch a new repository" class="icon">
+		<!-- @vue-ignore TODO remove when all underlying component migrated to composition api -->
 		<repository-picker data-test="owner-input" />
 	</badge>
 </template>
 
 <script setup lang="ts">
 import { useConfigurationStore } from '@/stores/configuration'
-import Badge from '@/components/badge/badge'
-import RepositoryPicker from '@/components/repository-picker/repository-picker'
+import Badge from '@/components/badge/badge.vue'
+import RepositoryPicker from '@/components/repository-picker/repository-picker.vue'
 
 const configurationEnabled = useConfigurationStore().configurationEnabled
 </script>
