a972332c5fdf8736694297e0d59b38dbc5c96cf3
udebellabre <ubald.debellabre@shodo-studio.io>
2023-07-30 21:29:10 +0200

    Removed useless setup file

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,5 +1,5 @@
 // TODO this hack is used because jsdom does not mock Notification API
 // see https://github.com/jsdom/jsdom/issues/1862
 window.Notification = {
-	permission: 'granted'
+	permission: 'denied'
 }
