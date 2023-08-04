d76a44ba66eaaadd638f2d7306be781a62af1659
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-05 01:20:30 +0200

    Rename pop-over component

--- src/components/popover/popover.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div :class="side" class="popover">
-		<slot />
-	</div>
-</template>
-
-<script src="./popover.js"></script>
-<style src="./popover.scss" scoped></style>
--- src/components/pull-request-line/pull-request-line.js
+++ src/components/pull-request-line/pull-request-line.js
@@ -2,7 +2,7 @@ import Badge from '../badge/badge.vue'
 import LivingIcon from '../living-icon/living-icon.vue'
 import UpdateIcon from '../update-icon/update-icon.vue'
 import BuildStatuses from '../build-statuses/build-statuses.vue'
-import Popover from '../popover/popover.vue'
+import Popover from '@/components/pop-over/pop-over.vue'
 
 export default {
 	name: 'pull-request-line',
