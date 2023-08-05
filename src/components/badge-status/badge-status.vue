1cb51bb59cb455d9a220592401bcc23ae621d914
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-05 17:56:43 +0200

    Move from sass to native css

--- src/components/badge-status/badge-status.vue
+++ src/components/badge-status/badge-status.vue
@@ -10,4 +10,29 @@ const emits = defineEmits<{
 }>()
 </script>
 
-<style src="./badge-status.scss" scoped></style>
+<style scoped lang="css">
+.badge {
+	background-color: var(--color-surface);
+
+	border-radius: 5px;
+	margin: 1px;
+	padding: 5px;
+}
+
+/* TODO move this to props instead of classes so that it can be type-checked */
+.SUCCESS {
+	background-color: var(--color-success);
+}
+
+.FAILURE {
+	background-color: var(--color-failure);
+}
+
+.PENDING {
+	background-color: var(--color-pending);
+}
+
+.badge:hover {
+	filter: brightness(150%);
+}
+</style>
