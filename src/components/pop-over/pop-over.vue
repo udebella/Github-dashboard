b832cc7e69184650dc731ec18f0b03ce579e5691
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-06 03:15:34 +0200

    Move from sass to css

--- src/components/pop-over/pop-over.vue
+++ src/components/pop-over/pop-over.vue
@@ -13,57 +13,48 @@ withDefaults(
 )
 </script>
 
-<style lang="scss" scoped>
-@import '../../global';
-
-$borderColor: $secondaryDark;
-$backgroundColor: $surface;
-
+<style lang="css" scoped>
 .popover {
 	position: absolute;
 	top: 50%;
 	transform: translateY(-50%);
-	background-color: $backgroundColor;
-	border: 1px solid $borderColor;
+	background-color: var(--color-surface);
+	border: 1px solid var(--color-secondaryDark);
 	border-radius: 5px;
 	z-index: 2;
 	padding: 10px;
 	box-shadow: 3px 3px 3px black;
+}
+.popover:before,
+.popover:after {
+	position: absolute;
+	top: calc(50% - 10px);
+	content: '';
+	border: 10px solid transparent;
+}
 
-	&.left {
-		right: calc(100% + 10px);
+.popover.left {
+	right: calc(100% + 10px);
+}
+.popover.left:before {
+	right: -20px;
+	border-left-color: var(--color-secondaryDark);
+}
+.popover.left:after {
+	right: -19px;
+	border-left-color: var(--color-surface);
+}
 
-		&:before {
-			right: -20px;
-			border-left-color: $borderColor;
-		}
+.popover.right {
+	left: calc(100% + 10px);
+}
+.popover.right:before {
+	left: -20px;
+	border-right-color: var(--color-secondaryDark);
+}
 
-		&:after {
-			right: -19px;
-			border-left-color: $backgroundColor;
-		}
-	}
-
-	&.right {
-		left: calc(100% + 10px);
-
-		&:before {
-			left: -20px;
-			border-right-color: $borderColor;
-		}
-
-		&:after {
-			left: -19px;
-			border-right-color: $backgroundColor;
-		}
-	}
-
-	&:before,
-	&:after {
-		position: absolute;
-		top: calc(50% - 10px);
-		content: '';
-		border: 10px solid transparent;
-	}
+.popover.right:after {
+	left: -19px;
+	border-right-color: var(--color-surface);
 }
 </style>
