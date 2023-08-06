7222f153c899e8011372b70e8e4c7d32a2939c0e
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-06 16:41:08 +0200

    Group tests in describes

--- src/services/pull-request/pull-request.spec.ts
+++ src/services/pull-request/pull-request.spec.ts
@@ -79,114 +79,118 @@ describe('Pull request service', () => {
 		])
 	})
 
-	it('returns a default lastEventAuthor when the last action is a commit', () => {
-		httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0].author = { login: 'udebella' }
+	describe('LastEventAuthor read', () => {
+		it('returns a default lastEventAuthor when the last action is a commit', () => {
+			httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0].author = { login: 'udebella' }
 
-		const response = extractHttp(httpResponse)
+			const response = extractHttp(httpResponse)
 
-		expect(response[0].lastEventAuthor).toBe('udebella')
-	})
+			expect(response[0].lastEventAuthor).toBe('udebella')
+		})
 
-	it('returns a default lastEventAuthor when the last action is not tracked', () => {
-		httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0] = {}
+		it('returns a default lastEventAuthor when the last action is not tracked', () => {
+			httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0] = {}
 
-		const response = extractHttp(httpResponse)
+			const response = extractHttp(httpResponse)
 
-		expect(response[0].lastEventAuthor).toBe('')
-	})
+			expect(response[0].lastEventAuthor).toBe('')
+		})
 
-	it('is able to retrieve lastEventAuthor from commits', () => {
-		httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0] = {
-			commit: { author: { user: { login: 'udebella' } } }
-		}
-
-		const response = extractHttp(httpResponse)
-
-		expect(response[0].lastEventAuthor).toBe('udebella')
-	})
-
-	it('extracts statuses from a response on a pull request that was built', () => {
-		httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
-			state: 'SUCCESS',
-			context: 'build description',
-			targetUrl: 'http://build-target-url'
-		}
-
-		const response = extractHttp(httpResponse)
-
-		expect(response).toEqual([
-			{
-				prTitle: 'Fix wheel/touch browser locking in IE and Safari',
-				prUrl: 'https://github.com/facebook/react/pull/9333',
-				creationDate: new Date('2018-10-20T00:00:00Z'),
-				updateDate: new Date('2018-10-25T01:36:27Z'),
-				lastEventAuthor: 'udebella',
-				buildStatus: 'SUCCESS',
-				statuses: [
-					{
-						jobStatus: 'SUCCESS',
-						description: 'build description',
-						jobUrl: 'http://build-target-url'
-					}
-				]
+		it('is able to retrieve lastEventAuthor from commits', () => {
+			httpResponse[0].pullRequests.nodes[0].timelineItems.nodes[0] = {
+				commit: { author: { user: { login: 'udebella' } } }
 			}
-		])
+
+			const response = extractHttp(httpResponse)
+
+			expect(response[0].lastEventAuthor).toBe('udebella')
+		})
 	})
 
-	it('extracts statuses from a response on a pull request that was built with github actions', () => {
-		httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
-			conclusion: 'SUCCESS',
-			name: 'build description',
-			detailsUrl: 'http://build-target-url'
-		}
-
-		const response = extractHttp(httpResponse)
-
-		expect(response).toEqual([
-			{
-				prTitle: 'Fix wheel/touch browser locking in IE and Safari',
-				prUrl: 'https://github.com/facebook/react/pull/9333',
-				creationDate: new Date('2018-10-20T00:00:00Z'),
-				updateDate: new Date('2018-10-25T01:36:27Z'),
-				lastEventAuthor: 'udebella',
-				buildStatus: 'SUCCESS',
-				statuses: [
-					{
-						jobStatus: 'SUCCESS',
-						description: 'build description',
-						jobUrl: 'http://build-target-url'
-					}
-				]
+	describe('Statuses read', () => {
+		it('extracts statuses from a response on a pull request that was built', () => {
+			httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
+				state: 'SUCCESS',
+				context: 'build description',
+				targetUrl: 'http://build-target-url'
 			}
-		])
-	})
 
-	it('has a pending status while job is not finished', () => {
-		httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
-			conclusion: null,
-			name: 'build description',
-			detailsUrl: 'http://build-target-url'
-		}
+			const response = extractHttp(httpResponse)
 
-		const response = extractHttp(httpResponse)
+			expect(response).toEqual([
+				{
+					prTitle: 'Fix wheel/touch browser locking in IE and Safari',
+					prUrl: 'https://github.com/facebook/react/pull/9333',
+					creationDate: new Date('2018-10-20T00:00:00Z'),
+					updateDate: new Date('2018-10-25T01:36:27Z'),
+					lastEventAuthor: 'udebella',
+					buildStatus: 'SUCCESS',
+					statuses: [
+						{
+							jobStatus: 'SUCCESS',
+							description: 'build description',
+							jobUrl: 'http://build-target-url'
+						}
+					]
+				}
+			])
+		})
 
-		expect(response).toEqual([
-			{
-				prTitle: 'Fix wheel/touch browser locking in IE and Safari',
-				prUrl: 'https://github.com/facebook/react/pull/9333',
-				creationDate: new Date('2018-10-20T00:00:00Z'),
-				updateDate: new Date('2018-10-25T01:36:27Z'),
-				lastEventAuthor: 'udebella',
-				buildStatus: 'SUCCESS',
-				statuses: [
-					{
-						jobStatus: 'PENDING',
-						description: 'build description',
-						jobUrl: 'http://build-target-url'
-					}
-				]
+		it('extracts statuses from a response on a pull request that was built with github actions', () => {
+			httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
+				conclusion: 'SUCCESS',
+				name: 'build description',
+				detailsUrl: 'http://build-target-url'
 			}
-		])
+
+			const response = extractHttp(httpResponse)
+
+			expect(response).toEqual([
+				{
+					prTitle: 'Fix wheel/touch browser locking in IE and Safari',
+					prUrl: 'https://github.com/facebook/react/pull/9333',
+					creationDate: new Date('2018-10-20T00:00:00Z'),
+					updateDate: new Date('2018-10-25T01:36:27Z'),
+					lastEventAuthor: 'udebella',
+					buildStatus: 'SUCCESS',
+					statuses: [
+						{
+							jobStatus: 'SUCCESS',
+							description: 'build description',
+							jobUrl: 'http://build-target-url'
+						}
+					]
+				}
+			])
+		})
+
+		it('has a pending status while job is not finished', () => {
+			httpResponse[0].pullRequests.nodes[0].commits.nodes[0].commit.statusCheckRollup.contexts.nodes[0] = {
+				conclusion: null,
+				name: 'build description',
+				detailsUrl: 'http://build-target-url'
+			}
+
+			const response = extractHttp(httpResponse)
+
+			expect(response).toEqual([
+				{
+					prTitle: 'Fix wheel/touch browser locking in IE and Safari',
+					prUrl: 'https://github.com/facebook/react/pull/9333',
+					creationDate: new Date('2018-10-20T00:00:00Z'),
+					updateDate: new Date('2018-10-25T01:36:27Z'),
+					lastEventAuthor: 'udebella',
+					buildStatus: 'SUCCESS',
+					statuses: [
+						{
+							jobStatus: 'PENDING',
+							description: 'build description',
+							jobUrl: 'http://build-target-url'
+						}
+					]
+				}
+			])
+		})
 	})
 
 	it('orders pull requests by last update date', () => {
