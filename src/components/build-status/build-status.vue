35678eb4ef065894a59cae2446f767280209c605
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-07 13:47:47 +0200

    Inline javascript

--- src/components/build-status/build-status.vue
+++ src/components/build-status/build-status.vue
@@ -4,5 +4,41 @@
 	</custom-button>
 </template>
 
-<script src="./build-status.js"></script>
+<script lang="js">
+import CustomButton from '../custom-button/custom-button.vue'
+import IconComponent from '../icon/icon-component.vue'
+
+const statusToIcon = {
+	FAILURE: 'warning',
+	ERROR: 'error',
+	PENDING: 'pending',
+	SUCCESS: 'success'
+}
+
+export default {
+	name: 'build-status',
+	props: {
+		description: {
+			type: String,
+			required: true
+		},
+		url: {
+			type: String
+		},
+		state: {
+			required: true,
+			type: String
+		}
+	},
+	computed: {
+		icon() {
+			return statusToIcon[this.state]
+		}
+	},
+	components: {
+		IconComponent,
+		CustomButton
+	}
+}
+</script>
 <style src="./build-status.scss" scoped></style>
