8c5d45db658511526cb111751f5c26a0b4c0003a
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-08 02:58:57 +0200

    Simplify notification service

--- src/services/notifications/notification.ts
+++ src/services/notifications/notification.ts
@@ -19,23 +19,17 @@ const defaultDependencies: Dependencies = {
 }
 
 export const notificationApi = ({ Notification, document }: Dependencies = defaultDependencies) => {
-	let authorizedNotification: Promise<boolean>
+	let authorizedNotification = Notification.permission === 'granted'
 
-	if (Notification.permission === 'denied') {
-		authorizedNotification = Promise.resolve(false)
-	} else if (Notification.permission === 'granted') {
-		authorizedNotification = Promise.resolve(true)
-	}
-
-	const notify = async (notification: string) => {
-		if (document.hidden && (await authorizedNotification)) {
+	const notify = (notification: string) => {
+		if (document.hidden && authorizedNotification) {
 			new Notification(notification)
 		}
 	}
 
-	const requestNotifications = () => {
-		authorizedNotification = Notification.requestPermission().then((userAnswer) => userAnswer === 'granted')
-		return authorizedNotification
+	const requestNotifications = async () => {
+		const userAnswer = await Notification.requestPermission()
+		authorizedNotification = userAnswer === 'granted'
 	}
 
 	return {
