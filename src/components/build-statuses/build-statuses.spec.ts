cde5a59f543cda1c1420eda5f4402e6207a0ece3
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-09 01:00:59 +0200

    Simplify tests

--- src/components/build-statuses/build-statuses.spec.ts
+++ src/components/build-statuses/build-statuses.spec.ts
@@ -5,47 +5,27 @@ import { beforeEach, describe, expect, it } from 'vitest'
 describe('BuildStatuses component', () => {
 	let buildStatuses: VueWrapper
 
-	describe('without status', () => {
-		beforeEach(() => {
-			buildStatuses = shallowMount(BuildStatuses, {
-				propsData: {
-					statuses: []
-				}
-			})
-		})
-
-		describe('Initialisation', () => {
-			it('should not display when there is no status', () => {
-				expect(buildStatuses.find('div').exists()).toBe(false)
-			})
-		})
+	beforeEach(() => {
+		buildStatuses = shallowMount(BuildStatuses, { props: { statuses: [] } })
 	})
 
-	describe('with statuses', () => {
-		beforeEach(() => {
-			buildStatuses = shallowMount(BuildStatuses, {
-				propsData: {
-					statuses: [
-						{
-							description: 'description',
-							jobUrl: 'http://build-url',
-							jobStatus: 'SUCCESS'
-						}
-					]
-				}
-			})
-		})
+	it('does not display when there is no status', () => {
+		expect(buildStatuses.find('div').exists()).toBe(false)
+	})
 
-		describe('Initialisation', () => {
-			it('should display statuses', () => {
-				const buildStatus = buildStatuses.findComponent({ name: 'build-status' })
-				expect(buildStatus.exists()).toBe(true)
-				expect(buildStatus.props()).toEqual({
-					description: 'description',
-					url: 'http://build-url',
-					state: 'SUCCESS'
-				})
-			})
+	it('display statuses', async () => {
+		const status = {
+			description: 'description',
+			jobUrl: 'http://build-url',
+			jobStatus: 'SUCCESS'
+		}
+		await buildStatuses.setProps({ statuses: [status] })
+
+		const buildStatus = buildStatuses.findComponent({ name: 'build-status' })
+		expect(buildStatus.props()).toEqual({
+			description: 'description',
+			url: 'http://build-url',
+			state: 'SUCCESS'
 		})
 	})
 })
