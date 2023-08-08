71a19b14fb0507315a00701e407b92779f1e48af
udebellabre <ubald.debellabre@shodo-studio.io>
2023-08-08 02:11:08 +0200

    Inline javascript

--- src/components/dashboard-header/dashboard-header.vue
+++ src/components/dashboard-header/dashboard-header.vue
@@ -11,5 +11,24 @@
 	</header>
 </template>
 
-<script src="./dashboard-header.js"></script>
+<script lang="js">
+import CustomButton from '../custom-button/custom-button.vue'
+import IconComponent from '../icon/icon-component.vue'
+import LoginInput from '../login-input/login-input.vue'
+import ConfigurationButton from '../configuration-button/configuration-button.vue'
+
+export default {
+	name: 'dashboard-header',
+	data: () => ({
+		icon: 'github'
+	}),
+	components: {
+		IconComponent,
+		LoginInput,
+		CustomButton,
+		ConfigurationButton
+	}
+}
+</script>
+
 <style src="./dashboard-header.scss" scoped></style>
