6f50c5467012ddb6ed5b119aa32b8d28a93dd2e8
udebella <ubald2b@gmail.com>
2023-08-09 14:17:37 +0200

    Improve display for login page

--- src/components/ApplicationLayout.vue
+++ src/components/ApplicationLayout.vue
@@ -1,3 +1,15 @@
 <template>
-	<router-view />
+	<div class="page">
+		<router-view />
+	</div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="css" scoped>
+.page {
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+}
+</style>
--- src/views/login-view.vue
+++ src/views/login-view.vue
@@ -1,18 +1,35 @@
 <template>
-	<div data-test="title">Github Dashboard</div>
-	<div data-test="helper">
-		You must generate a token to login using the
-		<a
-			href="https://github.com/settings/tokens/new?description=Github%20Dashboard&scopes=repo%2Cread%3Auser%2Cuser%3Aemail"
-			data-test="link"
-			>settings</a
-		>
-		of your github instance
+	<div class="container">
+		<div data-test="title" class="title">Github Dashboard</div>
+		<div data-test="helper">
+			You must generate a token to login using the
+			<a
+				href="https://github.com/settings/tokens/new?description=Github%20Dashboard&scopes=repo%2Cread%3Auser%2Cuser%3Aemail"
+				data-test="link"
+				>settings</a
+			>
+			of your github instance
+		</div>
+		<!-- @vue-ignore TODO remove when the component is typescript compliant -->
+		<login-input />
 	</div>
-	<!-- @vue-ignore TODO remove when the component is typescript compliant -->
-	<login-input />
 </template>
 
 <script setup lang="ts">
 import LoginInput from '../components/login-input/login-input.vue'
 </script>
+
+<style lang="css" scoped>
+.container {
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	align-items: stretch;
+	align-self: center;
+}
+.title {
+	font-size: 30px;
+	font-weight: bold;
+	align-self: center;
+}
+</style>
