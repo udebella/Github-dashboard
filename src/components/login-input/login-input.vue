ad4889a889759be526dd5fe2265c1e1498048928
udebella <ubald2b@gmail.com>
2023-08-10 00:02:07 +0200

    Migrate to script setup notation

--- src/components/login-input/login-input.vue
+++ src/components/login-input/login-input.vue
@@ -2,24 +2,17 @@
 	<debounced-input placeholder="Github token" data-test="input-token" type="password" @input="performLogin" />
 </template>
 
-<script lang="js">
+<script lang="js" setup>
 import { buildUserService } from '../../services/user/user'
 import DebouncedInput from '../debounced-input/debounced-input.vue'
 
-export default {
-	name: 'login-input',
-	props: {
-		userService: {
-			default: buildUserService
-		}
-	},
-	methods: {
-		async performLogin(value) {
-			await this.userService.login(value)
-		}
-	},
-	components: {
-		DebouncedInput
+const props = defineProps({
+	userService: {
+		default: buildUserService
 	}
+})
+
+async function performLogin(value) {
+	await props.userService.login(value)
 }
 </script>
