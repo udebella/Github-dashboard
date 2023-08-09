d042fc46589f2959a2b4a132f29f0b5432ed8d7e
udebella <ubald2b@gmail.com>
2023-08-09 13:38:12 +0200

    Display a title for the page

--- src/views/login-view.spec.ts
+++ src/views/login-view.spec.ts
@@ -3,6 +3,13 @@ import { describe, expect, it } from 'vitest'
 import LoginView from './login-view.vue'
 
 describe('Login view', () => {
+	it('displays the name of the application', () => {
+		const wrapper = shallowMount(LoginView)
+
+		const title = wrapper.find('[data-test=title]')
+		expect(title.text()).toBe('Github Dashboard')
+	})
+
 	it('displays a login input', () => {
 		const wrapper = shallowMount(LoginView)
 
--- src/views/login-view.vue
+++ src/views/login-view.vue
@@ -1,6 +1,8 @@
 <template>
+	<div data-test="title">Github Dashboard</div>
 	<login-input />
 </template>
+
 <script setup lang="ts">
 import LoginInput from '../components/login-input/login-input'
 </script>
