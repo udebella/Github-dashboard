77af4f41c884ec76d98f8b03e57d62b0e44d4316
udebella <ubald2b@gmail.com>
2023-08-09 22:47:48 +0200

    Inline javascript

--- src/components/login-input/login-input.vue
+++ src/components/login-input/login-input.vue
@@ -11,5 +11,44 @@
 	</div>
 </template>
 
-<script src="./login-input.js"></script>
+<script lang="js">
+import { buildUserService } from '../../services/user/user'
+import { NO_USER } from '../../services/session/session'
+import DebouncedInput from '../debounced-input/debounced-input.vue'
+import IconComponent from '../icon/icon-component.vue'
+
+export default {
+	name: 'login-input',
+	props: {
+		userService: {
+			default: buildUserService
+		}
+	},
+	data() {
+		return {
+			icon: 'user',
+			connectedUser: this.userService.connectedUser(),
+			NO_USER: NO_USER
+		}
+	},
+	computed: {
+		displayInputToken() {
+			return this.connectedUser === this.NO_USER
+		},
+		title() {
+			return this.displayInputToken ? 'You are not logged in' : `Logged in as ${this.connectedUser.login}`
+		}
+	},
+	methods: {
+		async performLogin(value) {
+			await this.userService.login(value)
+			this.connectedUser = this.userService.connectedUser()
+		}
+	},
+	components: {
+		IconComponent,
+		DebouncedInput
+	}
+}
+</script>
 <style src="./login-input.scss" scoped></style>
