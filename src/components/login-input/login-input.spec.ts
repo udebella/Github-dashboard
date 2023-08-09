02ffbd627f855cc4f8b9c4ba2b9738d146814636
udebella <ubald2b@gmail.com>
2023-08-10 00:14:34 +0200

    Remove should in tests names

--- src/components/login-input/login-input.spec.ts
+++ src/components/login-input/login-input.spec.ts
@@ -22,19 +22,19 @@ describe('Login component', () => {
 		})
 	})
 
-	it('should display an input text to enter token when there is no connected user in session', () => {
+	it('displays an input text to enter token when there is no connected user in session', () => {
 		const inputToken = login.findComponent({ name: 'debounced-input' })
 
 		expect(inputToken.attributes().placeholder).toBe('Github token')
 	})
 
-	it('should trigger a login when input changes', async () => {
+	it('triggers a login when input changes', async () => {
 		await login.findComponent({ name: 'debounced-input' }).vm.$emit('input', 'test')
 
 		expect(mocks.login).toHaveBeenCalledWith('test')
 	})
 
-	it('should use an input of type password to allow autocomplete from password managers', () => {
+	it('uses an input of type password to allow autocomplete from password managers', () => {
 		expect(login.attributes().type).toBe('password')
 	})
 })
