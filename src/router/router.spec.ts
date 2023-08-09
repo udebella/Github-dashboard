9563763897f12915a1b04ed893f838e92b413593
udebella <ubald2b@gmail.com>
2023-08-09 19:35:38 +0200

    Redirect to login page when not authenticated

--- src/router/router.spec.ts
+++ src/router/router.spec.ts
@@ -1,20 +1,39 @@
-import { beforeEach, describe, expect, it } from 'vitest'
-import type { RoutesNames } from './router'
+import { beforeEach, describe, expect, it, vitest } from 'vitest'
+import type { Dependencies, RoutesNames } from './router'
 import { createRouter } from './router'
 import type { Router } from 'vue-router'
+import type { Mocks } from '../test-utils'
+import { NO_USER } from '../services/session/session'
 
 describe('Router', () => {
 	let router: Router
+	let mocks: Mocks<Dependencies>
 	beforeEach(() => {
-		router = createRouter()
+		mocks = {
+			connectedUser: vitest.fn()
+		}
+		router = createRouter(mocks)
 	})
 
 	const routes: RoutesNames[] = ['home', 'configuration', 'login']
 	routes.forEach((name) => {
-		it(`should render the route ${name}`, async () => {
+		it(`renders the route ${name}`, async () => {
+			mocks.connectedUser.mockReturnValue({ login: 'connected user', token: 'token' })
+
 			await router.push({ name })
 
 			expect(router.currentRoute.value.name).toBe(name)
+		})
+	})
+
+	const protectedRoutes: RoutesNames[] = ['home', 'configuration']
+	protectedRoutes.forEach((name) => {
+		it(`redirect to login page when trying to navigate to ${name} being unauthenticated`, async () => {
+			mocks.connectedUser.mockReturnValue(NO_USER)
+
+			await router.push({ name })
+
+			expect(router.currentRoute.value.name).toBe('login')
 		})
 	})
 })
--- src/router/router.ts
+++ src/router/router.ts
@@ -2,6 +2,8 @@ import { createRouter as createVueRouter, createWebHashHistory } from 'vue-route
 import App from '../components/App.vue'
 import ConfigurationView from '../views/configuration-view.vue'
 import LoginView from '../views/login-view.vue'
+import { buildUserService } from '../services/user/user'
+import { NO_USER } from '../services/session/session'
 
 const routes = [
 	{
@@ -23,9 +25,30 @@ const routes = [
 
 export type RoutesNames = (typeof routes)[number]['name']
 
-export const createRouter = () => {
-	return createVueRouter({
+// TODO move this type to the proper place
+type User =
+	| {
+			login: string
+			token: string
+	  }
+	| typeof NO_USER
+
+export type Dependencies = {
+	connectedUser: () => User
+}
+
+const defaultDependencies = { connectedUser: buildUserService().connectedUser }
+export const createRouter = ({ connectedUser }: Dependencies = defaultDependencies) => {
+	const router = createVueRouter({
 		history: createWebHashHistory('/Github-dashboard/'),
 		routes: routes
 	})
+
+	router.beforeEach((to) => {
+		if (connectedUser() === NO_USER && to.name !== 'login') {
+			return 'login'
+		}
+	})
+
+	return router
 }
