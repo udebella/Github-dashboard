1e5274c404f7005a73c3d4b55e45fe6ea4a3210a
udebella <ubald2b@gmail.com>
2023-08-11 13:28:33 +0200

    Migrate component to typescript

--- src/components/living-icon/living-icon.vue
+++ src/components/living-icon/living-icon.vue
@@ -4,17 +4,14 @@
 	</div>
 </template>
 
-<script lang="js" setup>
+<script lang="ts" setup>
 import { differenceInWeeks, formatDistanceToNow } from 'date-fns'
 import IconComponent from '../icon/icon-component.vue'
 import { computed } from 'vue'
 
-const props = defineProps({
-	date: {
-		type: Date,
-		required: true
-	}
-})
+const props = defineProps<{
+	date: Date
+}>()
 
 const title = computed(() => formatDistanceToNow(props.date, { addSuffix: true }))
 const icon = computed(() => (differenceInWeeks(new Date(), props.date) < 1 ? 'living' : 'dead'))
