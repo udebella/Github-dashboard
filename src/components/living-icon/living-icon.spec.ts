cd9757b6ba82719f2bbc7a3ac7991d2c9e1eff3c
udebella <ubald2b@gmail.com>
2023-08-11 13:39:02 +0200

    Use a beforeEach to initialise tests

--- src/components/living-icon/living-icon.spec.ts
+++ src/components/living-icon/living-icon.spec.ts
@@ -1,30 +1,37 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, type VueWrapper } from '@vue/test-utils'
 import LivingIcon from './living-icon.vue'
 import { subWeeks } from 'date-fns'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 
 describe('LivingIcon component', () => {
-	describe('Initialization', () => {
-		it('should display a living icon', () => {
-			const livingIcon = shallowMount(LivingIcon, { propsData: { date: new Date() } })
-			const icon = livingIcon.findComponent('[data-test=icon]')
+	let livingIcon: VueWrapper
 
-			expect(icon.attributes().icon).toBe('living')
+	beforeEach(() => {
+		livingIcon = shallowMount(LivingIcon, { propsData: { date: new Date() } })
+	})
+
+	it('should display a living icon', () => {
+		const icon = livingIcon.findComponent('[data-test=icon]')
+
+		expect(icon.attributes().icon).toBe('living')
+	})
+
+	it('should display a skull icon when date is one week before today', async () => {
+		const lastWeekDate = subWeeks(new Date(), 1)
+		await livingIcon.setProps({
+			date: lastWeekDate
+		})
+		const icon = livingIcon.findComponent('[data-test=icon]')
+
+		expect(icon.attributes().icon).toBe('dead')
+	})
+
+	it('should display a title indicating time since given date', async () => {
+		const lastWeekDate = subWeeks(new Date(), 1)
+		await livingIcon.setProps({
+			date: lastWeekDate
 		})
 
-		it('should display a skull icon when date is one week before today', () => {
-			const lastWeekDate = subWeeks(new Date(), 1)
-			const livingIcon = shallowMount(LivingIcon, { propsData: { date: lastWeekDate } })
-			const icon = livingIcon.findComponent('[data-test=icon]')
-
-			expect(icon.attributes().icon).toBe('dead')
-		})
-
-		it('should display a title indicating time since given date', () => {
-			const lastWeekDate = subWeeks(new Date(), 1)
-			const livingIcon = shallowMount(LivingIcon, { propsData: { date: lastWeekDate } })
-
-			expect(livingIcon.attributes().title).toBe('7 days ago')
-		})
+		expect(livingIcon.attributes().title).toBe('7 days ago')
 	})
 })
