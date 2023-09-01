53a4632d0358e0425f14e152a681a71687b0fd8f
udebella <ubald2b@gmail.com>
2023-09-01 20:32:12 +0200

    Migrate component to script setup notation

--- src/components/ui/main-container/main-container.vue
+++ src/components/ui/main-container/main-container.vue
@@ -3,32 +3,24 @@
 		<github-api-config class="configuration" />
 		<div class="columns">
 			<repository-list class="repository-list" />
+			<!-- @vue-ignore TODO remove when migrated to composition api -->
 			<pull-request-list class="pull-request-list" />
 			<div class="viewer-pull-request-list">
+				<!-- @vue-ignore TODO remove when migrated to composition api -->
 				<viewer-pull-request-list />
+				<!-- @vue-ignore TODO remove when migrated to composition api -->
 				<recently-closed-pull-requests />
 			</div>
 		</div>
 	</div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import GithubApiConfig from '../../github-api-config/github-api-config.vue'
 import RepositoryList from '../../repository-list/repository-list.vue'
 import PullRequestList from '../../pull-request-list/pull-request-list.vue'
 import ViewerPullRequestList from '../../viewer-pull-request-list/viewer-pull-request-list.vue'
 import RecentlyClosedPullRequests from '../../recently-closed-pull-requests/recently-closed-pull-requests.vue'
-
-export default {
-	name: 'main-container',
-	components: {
-		GithubApiConfig,
-		RepositoryList,
-		PullRequestList,
-		ViewerPullRequestList,
-		RecentlyClosedPullRequests
-	}
-}
 </script>
 
 <style lang="scss" scoped>
