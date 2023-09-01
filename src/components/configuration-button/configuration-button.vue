497ff4e5a8c4a96aaae70dd0ca0eac20ad41eb98
udebella <ubald2b@gmail.com>
2023-09-01 19:43:44 +0200

    Inline javascript in vue file

--- src/components/configuration-button/configuration-button.vue
+++ src/components/configuration-button/configuration-button.vue
@@ -4,5 +4,31 @@
 	</custom-button>
 </template>
 
-<script src="./configuration-button.js"></script>
+<script>
+import CustomButton from '../ui/custom-button/custom-button.vue'
+import { useConfigurationStore } from '../../stores/configuration/configuration'
+import IconComponent from '../ui/icon/icon-component.vue'
+
+export default {
+	setup() {
+		const configurationStore = useConfigurationStore()
+		return { configurationStore }
+	},
+	name: 'configuration-button',
+	computed: {
+		configurationMode() {
+			return this.configurationStore.configurationEnabled ? 'enabled' : 'disabled'
+		}
+	},
+	methods: {
+		toggleConfiguration() {
+			this.configurationStore.toggleConfiguration()
+		}
+	},
+	components: {
+		IconComponent,
+		CustomButton
+	}
+}
+</script>
 <style src="./configuration-button.scss" scoped></style>
