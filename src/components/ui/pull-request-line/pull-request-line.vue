24c2424426d3a2c385989335128ff7f8ce61a94e
udebella <ubald2b@gmail.com>
2023-09-03 22:36:49 +0200

    Inline javascript

--- src/components/ui/pull-request-line/pull-request-line.vue
+++ src/components/ui/pull-request-line/pull-request-line.vue
@@ -15,5 +15,48 @@
 	</a>
 </template>
 
-<script src="./pull-request-line.js"></script>
+<script>
+import LivingIcon from '../living-icon/living-icon.vue'
+import UpdateIcon from '../update-icon/update-icon.vue'
+import BuildStatuses from '../build-statuses/build-statuses.vue'
+import BadgeStatus from '../badge-status/badge-status.vue'
+import PopOver from '../pop-over/pop-over.vue'
+
+export default {
+	name: 'pull-request-line',
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+		url: {
+			type: String,
+			required: true
+		},
+		buildStatus: {
+			type: String,
+			required: true
+		},
+		creationDate: {
+			type: Date,
+			required: true
+		},
+		hasUpdates: {
+			type: Boolean,
+			required: true
+		},
+		statusesList: {
+			type: Array,
+			default: () => []
+		}
+	},
+	components: {
+		BadgeStatus,
+		LivingIcon,
+		UpdateIcon,
+		BuildStatuses,
+		PopOver
+	}
+}
+</script>
 <style src="./pull-request-line.scss" scoped></style>
