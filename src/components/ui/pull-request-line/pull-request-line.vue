9dcc7e58245a3f6fa6c0b9108c26643a844d19c3
udebella <ubald2b@gmail.com>
2023-09-03 22:59:08 +0200

    Use script setup notation

--- src/components/ui/pull-request-line/pull-request-line.vue
+++ src/components/ui/pull-request-line/pull-request-line.vue
@@ -15,49 +15,25 @@
 	</a>
 </template>
 
-<script>
+<script lang="ts" setup>
 import LivingIcon from '../living-icon/living-icon.vue'
 import UpdateIcon from '../update-icon/update-icon.vue'
 import BuildStatuses from '../build-statuses/build-statuses.vue'
 import BadgeStatus from '../badge-status/badge-status.vue'
 import PopOver from '../pop-over/pop-over.vue'
+import type { GDBuildStatus } from '../../../services/statuses/extract-statuses'
 
-export default {
-	name: 'pull-request-line',
-	props: {
-		title: {
-			type: String,
-			required: true
-		},
-		url: {
-			type: String,
-			required: true
-		},
-		buildStatus: {
-			type: String,
-			required: true
-		},
-		creationDate: {
-			type: Date,
-			required: true
-		},
-		hasUpdates: {
-			type: Boolean,
-			required: true
-		},
-		statusesList: {
-			type: Array,
-			default: () => []
-		}
-	},
-	components: {
-		BadgeStatus,
-		LivingIcon,
-		UpdateIcon,
-		BuildStatuses,
-		PopOver
-	}
-}
+withDefaults(
+	defineProps<{
+		title: string
+		url: string
+		buildStatus: string
+		creationDate: Date
+		hasUpdates: boolean
+		statusesList?: GDBuildStatus[]
+	}>(),
+	{ statusesList: () => [] }
+)
 </script>
 <style lang="scss" scoped>
 @import '../../../global';
