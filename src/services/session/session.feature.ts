e3078e2869865d4bd279284d86d168df370c4b34
udebella <ubald2b@gmail.com>
2023-09-07 00:52:42 +0200

    Move feature tests to typescript

--- src/services/session/session.feature.ts
+++ src/services/session/session.feature.ts
@@ -5,15 +5,6 @@ describe('Session component test', () => {
 	it('should be save and retrieve an item in the session', () => {
 		const sessionService = buildSessionService()
 
-		sessionService.setUser('token')
-		const token = sessionService.getUser()
-
-		expect(token).toBe('token')
-	})
-
-	it('should allow to save objects in session', () => {
-		const sessionService = buildSessionService()
-
 		sessionService.setUser({
 			login: 'user',
 			token: 'token'
