6e06cac689dcd21b0e897b5b9f2a7d69defd9909
udebella <ubald2b@gmail.com>
2023-09-07 00:53:04 +0200

    Reformat code

--- src/services/session/session.feature.ts
+++ src/services/session/session.feature.ts
@@ -5,15 +5,9 @@ describe('Session component test', () => {
 	it('should be save and retrieve an item in the session', () => {
 		const sessionService = buildSessionService()
 
-		sessionService.setUser({
-			login: 'user',
-			token: 'token'
-		})
+		sessionService.setUser({ login: 'user', token: 'token' })
 		const token = sessionService.getUser()
 
-		expect(token).toEqual({
-			login: 'user',
-			token: 'token'
-		})
+		expect(token).toEqual({ login: 'user', token: 'token' })
 	})
 })
