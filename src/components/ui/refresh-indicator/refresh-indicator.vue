7ea4251dc1cf668d308d3cf570f410fa262cf0d2
udebella <ubald2b@gmail.com>
2025-04-01 13:34:51 +0200

    Inline javascript in the component

--- src/components/ui/refresh-indicator/refresh-indicator.vue
+++ src/components/ui/refresh-indicator/refresh-indicator.vue
@@ -2,5 +2,56 @@
 	<div data-test="counter" :class="freshness">{{ counter }}s ago</div>
 </template>
 
-<script lang="ts" src="./refresh-indicator.js"></script>
+<script lang="ts">
+export default {
+	name: 'refresh-indicator',
+	props: {
+		promise: {
+			type: Promise,
+			required: true
+		},
+		timeBetweenRefresh: {
+			type: Number,
+			required: true
+		}
+	},
+	data() {
+		return {
+			interval: setTimeout(() => undefined, 0),
+			counter: 0
+		}
+	},
+	watch: {
+		promise() {
+			this.promise.then(this.resetCounter)
+		}
+	},
+	created() {
+		this.interval = setInterval(this.incrementCounter, 1000)
+	},
+	computed: {
+		freshness() {
+			if (this.counter < this.timeBetweenRefresh) {
+				return 'fresh'
+			}
+			if (this.counter > 2 * this.timeBetweenRefresh) {
+				return 'outdated'
+			}
+			return 'old'
+		}
+	},
+	methods: {
+		incrementCounter() {
+			this.counter++
+		},
+		resetCounter() {
+			this.counter = 0
+		}
+	},
+	unmounted() {
+		clearInterval(this.interval)
+	}
+}
+</script>
+
 <style src="./refresh-indicator.scss" scoped></style>
