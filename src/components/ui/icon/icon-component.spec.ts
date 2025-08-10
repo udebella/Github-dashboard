ed2ca5bf4263aec7057858a187c7337dbb943a14
udebella <ubald2b@gmail.com>
2025-08-10 11:51:15 +0200

    Use beforeEach to remove test setup duplication

--- src/components/ui/icon/icon-component.spec.ts
+++ src/components/ui/icon/icon-component.spec.ts
@@ -1,20 +1,19 @@
-import { describe, expect, it } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { shallowMount, type VueWrapper } from '@vue/test-utils'
 import IconComponent from './icon-component.vue'
 
 describe('Icon component', () => {
-	it('displays an icon', () => {
-		const wrapper = shallowMount(IconComponent, {
-			props: {
-				icon: 'warning'
-			}
-		})
+	let wrapper: VueWrapper
 
+	beforeEach(() => {
+		wrapper = shallowMount(IconComponent, { props: { icon: 'warning' } })
+	})
+
+	it('displays an icon', () => {
 		expect(wrapper.find('i').attributes().class).toBe('ri-error-warning-fill')
 	})
-	it('has reactive icons', async () => {
-		const wrapper = shallowMount(IconComponent, { props: { icon: 'warning' } })
 
+	it('has reactive icons', async () => {
 		await wrapper.setProps({ icon: 'success' })
 
 		expect(wrapper.find('i').attributes().class).toBe('ri-checkbox-circle-fill')
