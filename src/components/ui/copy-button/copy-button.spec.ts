316c82e719f82cd7bc96907e3ed064ab06e55cca
udebella <ubald2b@gmail.com>
2025-08-10 11:36:35 +0200

    Remove should in tests

--- src/components/ui/copy-button/copy-button.spec.ts
+++ src/components/ui/copy-button/copy-button.spec.ts
@@ -25,28 +25,28 @@ describe('CopyButton component', () => {
 		vi.useRealTimers()
 	})
 
-	it('should display the component', () => {
+	it('displays the component', () => {
 		expect(copyButton.findComponent(CustomButton).text()).toBe('Copy to clipboard')
 	})
 
-	it('should copy value prop to clipboard', async () => {
+	it('copies value prop to clipboard', async () => {
 		await copyButton.trigger('click')
 
 		expect(mocks.clipboard.writeText).toHaveBeenCalledWith('value to copy')
 	})
 
 	describe('Icon', () => {
-		it('should display a clipboard icon', async () => {
+		it('displays a clipboard icon', async () => {
 			expect(copyButton.findComponent(CustomButton).findComponent(Icon).props()).toEqual({ icon: 'clipboard' })
 		})
 
-		it('should switch to success icon when copied', async () => {
+		it('switches to success icon when copied', async () => {
 			await copyButton.trigger('click')
 
 			expect(copyButton.findComponent(CustomButton).findComponent(Icon).props()).toEqual({ icon: 'success' })
 		})
 
-		it('should flip back to clipboard icon after 10 seconds', async () => {
+		it('flips back to clipboard icon after 10 seconds', async () => {
 			await copyButton.trigger('click')
 			await vi.advanceTimersByTime(10_000)
 
