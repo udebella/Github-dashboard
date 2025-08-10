b831b1190a0001f09068e0bbde657cc7ba930ed5
udebella <ubald2b@gmail.com>
2025-08-11 01:02:17 +0200

    Remove github api configuration input as it is in configuration route

--- src/components/ui/main-container/main-container.spec.ts
+++ src/components/ui/main-container/main-container.spec.ts
@@ -1,7 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import MainContainer from './main-container.vue'
 import { beforeEach, describe, expect, it } from 'vitest'
-import GithubApiConfig from '../../github-api-config/github-api-config.vue'
 import RepositoryList from '../../repository-list/repository-list.vue'
 import PullRequestList from '../../pull-request-list/pull-request-list.vue'
 import RecentlyClosedPullRequests from '../../recently-closed-pull-requests/recently-closed-pull-requests.vue'
@@ -15,10 +14,6 @@ describe('MainContainer component', () => {
 	})
 
 	describe('Initialization', () => {
-		it('should display configuration component', () => {
-			expect(mainContainer.findComponent(GithubApiConfig).exists()).toBe(true)
-		})
-
 		it('should display repository list component', () => {
 			expect(mainContainer.findComponent(RepositoryList).exists()).toBe(true)
 		})
