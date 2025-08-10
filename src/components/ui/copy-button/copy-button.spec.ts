ab99df0b0cdeadf29c564030595dd389266cabc8
udebella <ubald2b@gmail.com>
2025-08-10 11:10:52 +0200

    Update label for the button

--- src/components/ui/copy-button/copy-button.spec.ts
+++ src/components/ui/copy-button/copy-button.spec.ts
@@ -1,15 +1,15 @@
-import { describe, beforeEach, it, expect} from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { shallowMount, type VueWrapper } from '@vue/test-utils'
 import CopyButton from './copy-button.vue'
 
 describe('CopyButton component', () => {
-	let copyButton: VueWrapper;
+	let copyButton: VueWrapper
 
 	beforeEach(() => {
 		copyButton = shallowMount(CopyButton)
 	})
 
 	it('should display the component', () => {
-		expect(copyButton.text()).toBe('Test component')
+		expect(copyButton.text()).toBe('Copy to clipboard')
 	})
 })
--- src/components/ui/copy-button/copy-button.vue
+++ src/components/ui/copy-button/copy-button.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>Test component</div>
+	<div>Copy to clipboard</div>
 </template>
 
 <script lang="ts" setup></script>
