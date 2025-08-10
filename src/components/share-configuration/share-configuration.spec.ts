6b376ceae472c66ec1122b7e9e8d106ba0afd71d
udebella <ubald2b@gmail.com>
2025-08-10 11:42:40 +0200

    displays copy to clipboard component with share-string

--- src/components/share-configuration/share-configuration.spec.ts
+++ src/components/share-configuration/share-configuration.spec.ts
@@ -1,15 +1,19 @@
-import { describe, beforeEach, it, expect} from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { shallowMount, type VueWrapper } from '@vue/test-utils'
 import ShareConfiguration from './share-configuration.vue'
+import CopyButton from '../ui/copy-button/copy-button.vue'
+import { setActivePinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
 
 describe('ShareConfiguration component', () => {
-	let shareConfiguration: VueWrapper;
+	let shareConfiguration: VueWrapper
 
 	beforeEach(() => {
+		setActivePinia(createTestingPinia())
 		shareConfiguration = shallowMount(ShareConfiguration)
 	})
 
-	it('should display the component', () => {
-		expect(shareConfiguration.text()).toBe('Test component')
+	it('displays copy to clipboard component with share-string', () => {
+		expect(shareConfiguration.findComponent(CopyButton).props()).toEqual({ value: 'W10=' })
 	})
 })
--- src/components/share-configuration/share-configuration.vue
+++ src/components/share-configuration/share-configuration.vue
@@ -1,7 +1,12 @@
 <template>
-	<div>Test component</div>
+	<copy-button :value="store.shareString" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import CopyButton from '../ui/copy-button/copy-button.vue'
+import { useRepositoryStore } from '../../stores/repositories/repositories.ts'
+
+const store = useRepositoryStore()
+</script>
 
 <style lang="css" scoped></style>
