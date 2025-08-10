dce562230ebaa54abdbf62ca4cec0e2e14cbaff9
udebella <ubald2b@gmail.com>
2025-08-10 11:19:56 +0200

    should copy value prop to clipboard

--- src/components/ui/copy-button/copy-button.spec.ts
+++ src/components/ui/copy-button/copy-button.spec.ts
@@ -1,18 +1,28 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { shallowMount, type VueWrapper } from '@vue/test-utils'
 import CopyButton from './copy-button.vue'
 import CustomButton from '../custom-button/custom-button.vue'
+import type { Mocks } from '../../../test-utils.ts'
 
 describe('CopyButton component', () => {
 	let copyButton: VueWrapper
+	let mocks: Mocks<{ clipboard: { writeText: (text: string) => void } }>
 
 	beforeEach(() => {
+		mocks = { clipboard: { writeText: vi.fn() } }
 		copyButton = shallowMount(CopyButton, {
-			global: { renderStubDefaultSlot: true }
+			props: { value: 'value to copy' },
+			global: { provide: { clipboard: mocks.clipboard }, renderStubDefaultSlot: true }
 		})
 	})
 
 	it('should display the component', () => {
 		expect(copyButton.findComponent(CustomButton).text()).toBe('Copy to clipboard')
+	})
+
+	it('should copy value prop to clipboard', async () => {
+		await copyButton.trigger('click')
+
+		expect(mocks.clipboard.writeText).toHaveBeenCalledWith('value to copy')
 	})
 })
--- src/components/ui/copy-button/copy-button.vue
+++ src/components/ui/copy-button/copy-button.vue
@@ -1,9 +1,14 @@
 <template>
-	<CustomButton>Copy to clipboard</CustomButton>
+	<CustomButton @click="clipboard.writeText(value)">Copy to clipboard</CustomButton>
 </template>
 
 <script lang="ts" setup>
 import CustomButton from '../custom-button/custom-button.vue'
+import { inject } from 'vue'
+
+defineProps<{ value: string }>()
+
+const clipboard = inject('clipboard', navigator.clipboard)
 </script>
 
 <style lang="css" scoped></style>
