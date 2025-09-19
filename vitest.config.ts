abfa634167b0eedd09e710fa5512faf6886d52a3
udebella <ubald2b@gmail.com>
2025-09-19 09:39:50 +0200

    Use happy-dom instead of jsdom

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default mergeConfig(
 	viteConfig,
 	defineConfig({
 		test: {
-			environment: 'jsdom',
+			environment: 'happy-dom',
 			include: [...configDefaults.include, '**/*.feature.[jt]s'],
 			exclude: [...configDefaults.exclude, 'e2e/*'],
 			root: fileURLToPath(new URL('./', import.meta.url)),
