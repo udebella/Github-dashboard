4e4d845183ee4a326d069e6e240b10398e1c3f34
udebella <ubald2b@gmail.com>
2025-12-20 17:04:29 +0100

    Migrate component to script setup and typescript

--- src/components/repository-remover/repository-remover.vue
+++ src/components/repository-remover/repository-remover.vue
@@ -1,7 +1,25 @@
 <template>
-	<custom-button v-if="configurationEnabled" @click="remove">
+	<custom-button
+		v-if="configurationStore.configurationEnabled"
+		@click="repositoryStore.removeRepository({ name, owner })"
+	>
 		<icon-component :icon="icon" data-test="icon" />
 	</custom-button>
 </template>
 
-<script lang="ts" src="./repository-remover.js"></script>
+<script lang="ts" setup>
+import { useRepositoryStore } from '../../stores/repositories/repositories.ts'
+import { useConfigurationStore } from '../../stores/configuration/configuration.ts'
+import IconComponent from '../ui/icon/icon-component.vue'
+import CustomButton from '../ui/custom-button/custom-button.vue'
+
+const repositoryStore = useRepositoryStore()
+const configurationStore = useConfigurationStore()
+
+const icon = 'deleteBin'
+
+defineProps<{
+	name: string
+	owner: string
+}>()
+</script>
